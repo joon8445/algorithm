9826562d13fdd2e13906863e60c79c1173cd7e86
s-jun <joon8445@gmail.com>
2022-06-28 16:23:36 +0900

    Create 2116_주사위쌓기.java

---
+++ boj/2116_주사위쌓기.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		// 0:5 1:3 2:4
+		int[] match = {5,3,4,1,2,0};
+		
+		for(int i=0;i<N;i++) {
+			
+		}
+	}
+}
